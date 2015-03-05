a09d464e072890886bf206a9074edeca7f539d99
JohnnyPea <JohnnyPea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-05 23:15:49 +0000

    = 1.0.3 = * Tiny enhancements
    git-svn-id: https://plugins.svn.wordpress.org/camptix-trustcard/trunk@1106439 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- payment-trustcard.php
+++ payment-trustcard.php
@@ -44,9 +44,9 @@ class CampTix_Payment_Method_TrustCard extends CampTix_Payment_Method {
 	 * validate them all in validate_options.
 	 */
 	function payment_settings_fields() {
-		$this->add_settings_field_helper( 'merchant_id', __( 'Merchant AID', 'camptix' ), array( $this, 'field_text' ) );
-		$this->add_settings_field_helper( 'secure_key', __( 'Secret key', 'camptix' ), array( $this, 'field_text' ) );
-		$this->add_settings_field_helper( 'is_test', __( 'Test mode', 'camptix' ), array( $this, 'field_yesno' ) );
+		$this->add_settings_field_helper( 'merchant_id', __( 'Merchant AID', 'camptix_trustcard' ), array( $this, 'field_text' ) );
+		$this->add_settings_field_helper( 'secure_key', __( 'Secret key', 'camptix_trustcard' ), array( $this, 'field_text' ) );
+		$this->add_settings_field_helper( 'is_test', __( 'Test mode', 'camptix_trustcard' ), array( $this, 'field_yesno' ) );
 	}
 
 	/**
@@ -89,8 +89,8 @@ class CampTix_Payment_Method_TrustCard extends CampTix_Payment_Method {
 		$msg = "TrustPay Card Payments response data:\n";
         $msg .= "AID=".(isset($response->AID) ? esc_html($response->AID) : '');
         $msg .= " &CardID=".(isset($response->CardID) ? esc_html($response->CardID) : '');
-        $msg .= " &CardMask=".(isset($response->CardMask) ? esc_html($response->CardMask) : '');
-        $msg .= " &CardExp=".(isset($response->CardExp) ? esc_html($response->CardExp) : '');
+        //$msg .= " &CardMask=".(isset($response->CardMask) ? esc_html($response->CardMask) : '');
+        //$msg .= " &CardExp=".(isset($response->CardExp) ? esc_html($response->CardExp) : '');
         $msg .= " &AuthNumber=".(isset($response->AuthNumber) ? esc_html($response->AuthNumber) : '');
         $msg .= " &CardRecTxSec=".(isset($response->CardRecTxSec) ? esc_html($response->CardRecTxSec) : '');
         $msg .= " &TSS2=".(isset($response->TSS2) ? esc_html($response->TSS2): '');
@@ -161,7 +161,7 @@ class CampTix_Payment_Method_TrustCard extends CampTix_Payment_Method {
 		$currency = $this->camptix_options['currency'];
 
 		if ( ! in_array( $currency, $this->supported_currencies ) )
-			die( __( 'The selected currency is not supported by this payment method.', 'camptix' ) );
+			die( __( 'The selected currency is not supported by this payment method.', 'camptix_trustcard' ) );
 
 		$is_test = (!empty($this->options['is_test']) && $this->options['is_test']) ? true : false;
 
